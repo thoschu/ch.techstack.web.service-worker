0d0e2fd6e9f2c9c1f113bbdb735330dd41c47349
Tom S <thoschulte@gmail.com>
2018-06-25 16:45:15 +0200

    caching

--- sw.js
+++ sw.js
@@ -1,32 +1,19 @@
+var version = 'v1.0.3::';
+
 self.addEventListener('install', function (event) {
-    var root = event.target.self.registration.scope;
-    var version = 'v1::';
-    console.log('install');
-    // Perform install steps
-    /*  The caches built-in is a promise-based API that helps to cache responses,
-        as well as finding and deleting them.
-    */
-    var cachePromise = caches
-    /*  You can open a cache by name, and this method returns a promise. We use
-        a versioned cache name here so that we can remove old cache entries in
-        one fell swoop later, when phasing out an older service worker.
-    */
-        .open(version + 'test-site-cache-v1');
+    var cachePromise = caches.open(version + 'site-cache-v1');
 
     event.waitUntil(cachePromise
         .then(function (cache) {
             console.log('Opened cache');
-            /* After the cache is opened, fill it with the offline fundamentals.
-               The method below will add all resources which are indicated to the cache,
-               after making HTTP requests for each of them.
-            */
             return cache.addAll([
-                '/',
-                '/css/main.css',
-                '/js/test.js'
+                './',
+                './css/main.css',
+                './js/test.js'
             ]);
-        }).then(function () {
+        }).then(function (cache) {
             console.log('SERVICE WORKER: install completed');
+            return cache;
         }).catch(function (err) {
             console.log(err);
         })
@@ -35,15 +22,9 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('fetch', function (event) {
     var eventReq = event.request;
-    console.log('fetch');
-    console.log(event.request.url);
-
     // event.respondWith(new Response("Das ist ein neuer Inhalt"));
 
-    if (event.request.method !== 'GET') {
-        /* If it don't block the event as shown below, then the request will go to
-           the network as usual.
-        */
+    if (eventReq.method !== 'GET') {
         console.log('WORKER: fetch event ignored.', eventReq.method, eventReq.url);
     } else {
         if (eventReq.url.indexOf(".bar") >= 0) {
@@ -53,26 +34,66 @@ self.addEventListener('fetch', function (event) {
                 })
             );
         } else {
-            var promise = caches.match(event.request);
             event.respondWith(
-                promise.then(function (response) {
+                caches.match(event.request).then(function (response) {
+                    console.log(event.request);
+                    console.log(response);
+
                     // Cache hit - return response
                     if (response) {
+                        console.log(">>>>>>>");
+                        console.log(response);
+
                         return response;
                     } else {
-                        var fetchResult = fetch(event.request);
                         console.log("!!!!!!!!");
-                        console.dir(fetchResult);
-                        return fetchResult;
+
+                        fetch(event.request).then(function (response) {
+
+                            console.log('WORKER: fetch response from network.', event.request.url);
+                            var cacheCopy = response.clone();
+                            var pagesPromise = caches.open(version + 'pages');
+
+                            pagesPromise.then(function add(cache) {
+                                cache.put(event.request, cacheCopy);
+                                return cache;
+                            }).then(function (cache) {
+                                console.log('WORKER: fetch response stored in cache.', event.request.url);
+                                return fetch(event.request);
+                            });
+                        });
+
+                        //return fetch(event.request);
                     }
-                }).catch(function (err) {
-                    console.error(err);
+                }).catch((err) => {
+                    console.error('WORKER: fetch request failed in both cache and network: ', err);
+                    return new Response('<h1>Service Unavailable</h1>', {
+                        status: 503,
+                        statusText: 'Service Unavailable',
+                        headers: new Headers({
+                            'Content-Type': 'text/html'
+                        })
+                    });
                 })
             );
         }
     }
 });
 
-self.addEventListener('activate', function (event) {
-    console.log('activate');
+self.addEventListener("activate", function (event) {
+    console.log('WORKER: activate event in progress.');
+
+    event.waitUntil(
+        caches.keys().then(function (keys) {
+            var promises = keys.filter(function (key) {
+                return !key.startsWith(version);
+            }).map(function (key) {
+                return caches.delete(key);
+            });
+
+            return Promise.all(promises);
+        }).then(function () {
+            console.log('WORKER: activate completed.');
+        })
+    );
 });

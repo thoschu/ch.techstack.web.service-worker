f4b5ad6126a494bfe9be054bc67982fd6c703790
Tom S <thoschulte@gmail.com>
2018-06-26 17:22:34 +0200

    caching

--- index.js
+++ index.js
@@ -1,124 +1,79 @@
-// const cluster = require('cluster');
-// const http = require('http');
-// const os = require('os');
-// const R = require('ramda');
-// const webSocket = require('websocket');
-//
-// if (cluster.isMaster) {
-//     const numCPUs = os.cpus();
-//     console.log(`Master ${process.pid} is running`);
-//
-//     // Fork workers.
-//     R.forEach((cpu) => {
-//         cluster.fork();
-//     }, numCPUs);
-//
-//     cluster.on('exit', (worker, code, signal) => {
-//         console.log(`${code}: worker ${worker.process.pid} died with signal: ${signal}`);
-//     });
-// } else {
-//     const WebSocketServer = webSocket.server;
-//
-//     const server = http.createServer((request, response) => {
-//         console.log((new Date()) + ' Received request for ' + request.url);
-//         response.writeHead(404);
-//         response.end();
-//     });
-//     server.listen(1337, () => {
-//         console.log((new Date()) + ' Server is listening on port 1337');
-//     });
-//
-//     const wsServer = new WebSocketServer({
-//         httpServer: server,
-//         // You should not use autoAcceptConnections for production
-//         // applications, as it defeats all standard cross-origin protection
-//         // facilities built into the protocol and the browser.  You should
-//         // *always* verify the connection's origin and decide whether or not
-//         // to accept it.
-//         autoAcceptConnections: false
-//     });
-//
-//     const originIsAllowed = (origin) => {
-//         // put logic here to detect whether the specified origin is allowed.
-//         console.log(origin);
-//         return true;
-//     };
-//
-//     wsServer.on('request', (request) => {
-//         if (!originIsAllowed(request.origin)) {
-//             // Make sure we only accept requests from an allowed origin
-//             request.reject();
-//             console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-//         } else {
-//             const connection = request.accept('echo-protocol', request.origin);
-//             console.log((new Date()) + ' Connection accepted.');
-//             connection.on('message', (message) => {
-//                 if (message.type === 'utf8') {
-//                     console.log('Received Message: ' + message.utf8Data);
-//                     connection.sendUTF(message.utf8Data);
-//                 } else if (message.type === 'binary') {
-//                     console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-//                     connection.sendBytes(message.binaryData);
-//                 } else {}
-//             });
-//             connection.on('close', (reasonCode, description) => {
-//                 console.log((new Date()) + reasonCode + ': Peer ' + connection.remoteAddress + ' disconnected: ' + description);
-//             });
-//         }
-//     });
-//
-//     console.log(`Worker ${process.pid} started`);
-// }
+const cluster = require('cluster');
+const http = require('http');
+const os = require('os');
+const R = require('ramda');
+const webSocket = require('websocket');
 
+if (cluster.isMaster) {
+    const numCPUs = os.cpus();
+    console.log(`Master ${process.pid} is running`);
 
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+    // Fork workers.
+    R.forEach((cpu) => {
+        cluster.fork();
+    }, numCPUs);
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`${code}: worker ${worker.process.pid} died with signal: ${signal}`);
+    });
+} else {
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
+    // const WebSocketServer = webSocket.server;
+    //
+    // const server = http.createServer((request, response) => {
+    //     console.log((new Date()) + ' Received request for ' + request.url);
+    //     response.writeHead(404);
+    //     response.end();
+    // });
+    // server.listen(1337, () => {
+    //     console.log((new Date()) + ' Server is listening on port 1337');
+    // });
+    //
+    // const wsServer = new WebSocketServer({
+    //     httpServer: server,
+    //     // You should not use autoAcceptConnections for production
+    //     // applications, as it defeats all standard cross-origin protection
+    //     // facilities built into the protocol and the browser.  You should
+    //     // *always* verify the connection's origin and decide whether or not
+    //     // to accept it.
+    //     autoAcceptConnections: false
+    // });
+    //
+    // const originIsAllowed = (origin) => {
+    //     // put logic here to detect whether the specified origin is allowed.
+    //     console.log(origin);
+    //     return true;
+    // };
+    //
+    // wsServer.on('request', (request) => {
+    //     if (!originIsAllowed(request.origin)) {
+    //         // Make sure we only accept requests from an allowed origin
+    //         request.reject();
+    //         console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    //     } else {
+    //         const connection = request.accept('echo-protocol', request.origin);
+    //         console.log((new Date()) + ' Connection accepted.');
+    //         connection.on('message', (message) => {
+    //             if (message.type === 'utf8') {
+    //                 console.log('Received Message: ' + message.utf8Data);
+    //                 connection.sendUTF(message.utf8Data);
+    //             } else if (message.type === 'binary') {
+    //                 console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+    //                 connection.sendBytes(message.binaryData);
+    //             } else {}
+    //         });
+    //         connection.on('close', (reasonCode, description) => {
+    //             console.log((new Date()) + reasonCode + ': Peer ' + connection.remoteAddress + ' disconnected: ' + description);
+    //         });
+    //     }
+    // });
 
-function originIsAllowed(origin) {
-    // put logic here to detect whether the specified origin is allowed.
-    return true;
+    //create a server object:
+    http.createServer(function (req, res) {
+        res.write('### ONLINE ###'); //write a response to the client
+        res.end(); //end the response
+    }).listen(8088); //the server object listens on port 8088
+
+    console.log(`Worker ${process.pid} started`);
+
 }
-
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-        // Make sure we only accept requests from an allowed origin
-        request.reject();
-        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-        return;
-    }
-
-    var connection = request.accept('echo-protocol', request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
-});
--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
-var version = 'v1.0.14::';
+console.log("SW Startup!");
 
+var version = 'v1.0.22::';
+
+// Install Service Worker
 self.addEventListener('install', function (event) {
     event.waitUntil(caches.open(version + 'site-cache-beta')
         .then(function (cache) {
             console.log('Opened cache');
-            return cache.addAll([
+
+            cache.addAll([
                 './',
                 './css/main.css',
-                './js/test.js'
+                './js/test.js',
+                'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js'
             ]);
-        }).then(function () {
+
+            return cache;
+        }).then(function (cache) {
             console.log('SERVICE WORKER: install completed');
 
-
-
+            cache.keys().then(function (cacheNames) {
+                console.dir(cacheNames);
+            });
 
             return self.skipWaiting();
         }).catch(function (err) {
@@ -22,6 +30,7 @@ self.addEventListener('install', function (event) {
     );
 });
 
+// Service Worker Active
 self.addEventListener("activate", function (event) {
     console.log('WORKER: activate event in progress.');
 
@@ -41,10 +50,11 @@ self.addEventListener("activate", function (event) {
 
 self.addEventListener('fetch', function (event) {
     var eventReq = event.request;
-    // event.respondWith(new Response("Das ist ein neuer Inhalt"));
+    // console.log(eventReq.headers);
 
     if (eventReq.method !== 'GET') {
         console.log('WORKER: fetch event ignored.', eventReq.method, eventReq.url);
+        event.respondWith(new Response("Das ist kein gültiger Inhalt..."));
     } else {
         if (eventReq.url.indexOf(".bar") >= 0) {
             event.respondWith(
@@ -56,25 +66,28 @@ self.addEventListener('fetch', function (event) {
             event.respondWith(
                 caches.match(event.request).then(function (response) {
                     // Cache hit - return response
-                    if (response) {
-                        return response;
-                    } else {
-                        fetch(event.request).then(function (response) {
-                            console.log('WORKER: fetch response from network.', event.request.url);
-                            var eventRequest = event.request;
-                            var cacheCopy = response.clone();
-                            caches.open(version + 'pages').then(function (cache) {
-                                cache.put(eventRequest, cacheCopy);
-                                return;
-                            }).then(function () {
-                                console.log('WORKER: fetch response stored in cache.', event.request.url);
-                            });
-                        });
+                    return response;
 
-                        return fetch(event.request);
-                    }
+                    // if (response) {
+                    //     console.log('###############' + response);
+                    //     return response;
+                    // } else {
+                    //     fetch(event.request).then(function (response) {
+                    //         console.log('WORKER: fetch response from network.', event.request.url);
+                    //         var eventRequest = event.request;
+                    //         var cacheCopy = response.clone();
+                    //         caches.open(version + 'pages-cache-beta').then(function (cache) {
+                    //             cache.put(eventRequest, cacheCopy);
+                    //             return;
+                    //         }).then(function () {
+                    //             console.log('WORKER: fetch response stored in cache.', event.request.url);
+                    //         });
+                    //     });
+                    //
+                    //     return fetch(event.request);
+                    // }
                 }).catch((err) => {
-                    console.error('WORKER: fetch request failed in both cache and network: ', err);
+                    console.error('WORKER: fetch request failed in both: cache and network: ', err);
                     return new Response('<h1>Service Unavailable</h1>', {
                         status: 503,
                         statusText: 'Service Unavailable',
@@ -86,4 +99,64 @@ self.addEventListener('fetch', function (event) {
             );
         }
     }
+});
+
+self.addEventListener('sync', (event) => {
+    if (event.tag == 'eventx') {
+        event.waitUntil(doSomething());
+    }
+
+    function doSomething() {
+        return new Promise(
+            function (resolve, reject) {
+                var interval = setInterval(timerFn, 3000);
+
+                function timerFn() {
+                    fetch('http://localhost:8088', {
+                        mode: "no-cors"
+                    })
+                    .then(function (response) {
+                        console.log(response);
+                        clearInterval(interval);
+                        resolve(response);
+                    })
+                    .catch(function (error) {
+                        console.error('### OFFLINE ###');
+                        reject(error);
+                    });
+                }
+            }
+        );
+    }
+});
+
+self.addEventListener('message', async function (event) {
+    console.log("SW Received Message: " + event.data);
+    const client = event.source;
+    client.postMessage(`Pong: ${ event.data }`);
+
+    const clients = await self.clients.matchAll();
+
+    for (const c of clients) {
+        if (c !== client) {
+            console.log(c);
+            //c.postMessage(`Message from ${ messageSource.id }: ${ evt.data }`);
+        }
+    }
+
+}, false);
+
+self.addEventListener('push', function (event) {
+    console.log('Received a push event', event);
+
+    const options = {
+        title: 'I got a message for you!',
+        body: 'Here is the body of the message',
+        icon: '/img/icon-192x192.png',
+        tag: 'tag-for-this-notification',
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(event.data, options)
+    )
 });

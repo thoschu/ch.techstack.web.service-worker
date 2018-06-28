df0274c4ed2bf106fdd0a523527ec3d48ceac5a9
Tom S <thoschulte@gmail.com>
2018-06-28 09:45:33 +0200

    caching

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("SW Startup!");
 
-var version = 'v1.0.22::';
+var version = 'v1.4.1::';
 
 // Install Service Worker
 self.addEventListener('install', function (event) {
@@ -9,9 +9,11 @@ self.addEventListener('install', function (event) {
             console.log('Opened cache');
 
             cache.addAll([
-                './',
+                './index.html',
                 './css/main.css',
                 './js/test.js',
+                './img/offline/offline.png',
+                './img/offline/diamant-offline.gif',
                 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js'
             ]);
 
@@ -25,7 +27,7 @@ self.addEventListener('install', function (event) {
 
             return self.skipWaiting();
         }).catch(function (err) {
-            console.log(err);
+            console.error(err);
         })
     );
 });
@@ -52,56 +54,65 @@ self.addEventListener('fetch', function (event) {
     var eventReq = event.request;
     // console.log(eventReq.headers);
 
-    if (eventReq.method !== 'GET') {
+    if (eventReq.method !== 'GET' && event.request.mode === 'navigate') {
         console.log('WORKER: fetch event ignored.', eventReq.method, eventReq.url);
-        event.respondWith(new Response("Das ist kein gültiger Inhalt..."));
+        event.respondWith(new Response("Das ist kein gültiger Inhalt, da falsche HTTP-Verb..."));
     } else {
         if (eventReq.url.indexOf(".bar") >= 0) {
+            const imgRequest = new Request("http://animaticons.co/wp-content/uploads/animat-diamond-color.gif");
+
             event.respondWith(
-                fetch("http://i43.tinypic.com/rit92a.jpg", {
+                fetch(imgRequest, {
                     mode: "no-cors"
+                }).catch(function(err) {
+                    return caches.match('./img/offline/diamant-offline.gif');
                 })
             );
         } else {
             event.respondWith(
-                caches.match(event.request).then(function (response) {
-                    // Cache hit - return response
-                    return response;
+                fetch(event.request).then(function (response) {
+                    var cacheCopy = response.clone();
 
-                    // if (response) {
-                    //     console.log('###############' + response);
-                    //     return response;
-                    // } else {
-                    //     fetch(event.request).then(function (response) {
-                    //         console.log('WORKER: fetch response from network.', event.request.url);
-                    //         var eventRequest = event.request;
-                    //         var cacheCopy = response.clone();
-                    //         caches.open(version + 'pages-cache-beta').then(function (cache) {
-                    //             cache.put(eventRequest, cacheCopy);
-                    //             return;
-                    //         }).then(function () {
-                    //             console.log('WORKER: fetch response stored in cache.', event.request.url);
-                    //         });
-                    //     });
-                    //
-                    //     return fetch(event.request);
-                    // }
-                }).catch((err) => {
-                    console.error('WORKER: fetch request failed in both: cache and network: ', err);
-                    return new Response('<h1>Service Unavailable</h1>', {
-                        status: 503,
-                        statusText: 'Service Unavailable',
-                        headers: new Headers({
-                            'Content-Type': 'text/html'
-                        })
+                    caches.open(version + 'pages-cache-beta').then(function (cache) {
+                        console.log('WORKER: fetch response stored in cache.', event.request.url);
+                        cache.put(eventReq, cacheCopy);
                     });
+
+                    return response;
+                }).catch(err => {
+                    console.log(err);
+
+                    return caches.match(event.request).catch(function (err) {
+                        console.error(err);
+                        return new Response('<h1>Service Unavailable</h1>', {
+                            status: 503,
+                            statusText: 'Service Unavailable',
+                            headers: new Headers({
+                                'Content-Type': 'text/html'
+                            })
+                        });
+                    });
+
                 })
             );
         }
     }
 });
 
+
 self.addEventListener('sync', (event) => {
+    if (event.tag === 'image-fetch') {
+        event.waitUntil(function () {
+            fetch('http://animaticons.co/wp-content/uploads/animat-diamond-color.gif').then(function (response) {
+                return response;
+            }).then(function (text) {
+                console.log('Request successful', text);
+            }).catch(function (error) {
+                console.log('Request failed', error);
+            });
+        });
+    }
+    
     if (event.tag == 'eventx') {
         event.waitUntil(doSomething());
     }
